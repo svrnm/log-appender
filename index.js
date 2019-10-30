e4f6d867ee771a59119e6c13db08e3154b42ba39
Severin Neumann <severin.neumann@altmuehlnet.de>
2019-10-30 16:00:45 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const multer  = require('multer')
+const fs = require('fs');
+const fsp = fs.promises;
+const path = require('path');
+const sanitize = require("sanitize-filename");
+const morgan = require('morgan')
+
+const port = process.env.PORT ? process.env.PORT : 3131
+const dest = process.env.UPLOAD_FOLDER ? process.env.UPLOAD_FOLDER : '/tmp'
+
+const app = express();
+const upload = multer({ dest })
+
+app.use(morgan('combined'))
+
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.put('/stream/:name', (req, res) => {
+  name = sanitize(req.params.name)
+  if(name !== req.params.name) {
+    res.redirect(`/stream/${name}`)
+    return
+  }
+  var stream = fs.createWriteStream(`uploads/{name}`, {flags: 'a'});
+  // res.write(`${name} uploaded`)
+  req.on('data', chunck => {
+    stream.write(chunck)
+  })
+  req.on('end', (chunck) => {
+    res.end(`${name} uploaded`)
+  })
+  res.on('finish', () => {
+    stream.close()
+  })
+})
+
+app.post('/upload', upload.any(), async (req, res) => {
+  const result = await Promise.all(req.files.map(async (file) => {
+    const data = await fsp.readFile(file.path)
+    await fsp.appendFile(path.join(dest, file.originalname), data)
+    await fsp.unlink(file.path)
+    return `${file.originalname} uploaded`
+  }))
+  res.json(result)
+});
+
+app.listen(port, function () {
+  console.log(`log-appender listening on port ${port}! You can now upload files to ${dest}`);
+});
